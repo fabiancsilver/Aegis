104d84a29e2eb95f225833f3da92c15647be1a2b
Fabian Silvestre <fsilvestre@prismview.com>
2021-12-20 22:41:21 -0700

    Adds automap

---
+++ Aegis.AddressBook.API/Profiles/ContactProfile.cs
@@ -0,0 +1,14 @@
+﻿using Aegis.AddressBook.API.Models;
+using Aegis.AddressBook.Domain;
+using AutoMapper;
+
+namespace Aegis.AddressBook.API.Profiles
+{
+    public class ContactProfile : Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<Contact, ContactViewModel>().ReverseMap();
+        }
+    }
+}
